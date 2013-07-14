426c10fece363a885eb3da27d45c7e7a22e12a72
Ubuntu <ubuntu@ip-172-31-41-147.us-west-2.compute.internal>
2013-07-14 15:02:18 +0000

    Hello world2!

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
@@ -11,4 +11,3 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-//test
